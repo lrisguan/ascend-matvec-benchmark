48f8c8cd8f5400159c4e73bca69a7c54fc6cb437
lrisguan <lrisguan@outlook.com>
2025-12-21 16:39:28 +0800

    main_cpu.cpp: add average test

--- cpu/main_cpu.cpp
+++ cpu/main_cpu.cpp
@@ -5,25 +5,35 @@
 constexpr int M = 2048;
 constexpr int N = 2048;
 
-int main() {
-    std::vector<int> A(M * N, 1);
-    std::vector<int> B(N, 1);
-    std::vector<int> C(M, 0);
-
-    auto start = std::chrono::high_resolution_clock::now();
-
-    for (int i = 0; i < M; ++i) {
-        int sum = 0;
-        for (int j = 0; j < N; ++j) {
-            sum += A[i * N + j] * B[j];
-        }
-        C[i] = sum;
+inline void matvec(const std::vector<float>& A, const std::vector<float>& B, std::vector<float>& C) {
+  for (int i = 0; i < M; ++i) {
+    float sum = 0;
+    for (int j = 0; j < N; ++j) {
+      sum += A[i * N + j] * B[j];
     }
+    C[i] = sum;
+  }
+}
 
-    auto end = std::chrono::high_resolution_clock::now();
-    std::cout << "CPU Time: "
-              << std::chrono::duration<double, std::milli>(end - start).count()
-              << " ms\n";
+int main() {
+  std::vector<float> A(M * N, 1);
+  std::vector<float> B(N, 1);
+  std::vector<float> C(M, 0);
 
-    return 0;
+  // warm-up
+  matvec(A, B, C);
+  
+  auto start = std::chrono::high_resolution_clock::now();
+
+  // Perform matrix-vector multiplication 10000 times
+  for (int k = 0; k < 10000; ++k) {
+    matvec(A, B, C);
+  }
+
+  auto end = std::chrono::high_resolution_clock::now();
+  std::cout << "CPU MatVec Average Time: "
+            << std::chrono::duration<double, std::milli>(end - start).count() / 10000
+            << " ms\n";
+
+  return 0;
 }
