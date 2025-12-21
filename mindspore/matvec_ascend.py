37c4b376390da2fcbfbe420cb7602f56bc141343
lrisguan <lrisguan@outlook.com>
2025-12-21 16:40:02 +0800

    mindspore: add average test

--- mindspore/matvec_ascend.py
+++ mindspore/matvec_ascend.py
@@ -1,15 +1,23 @@
+import time
+import warnings
+import os
+warnings.filterwarnings("ignore", category=UserWarning, module="numpy")
+warnings.filterwarnings("ignore", message=".*device_target.*will be deprecated.*")
+os.environ['GLOG_minloglevel'] = '2'
 import mindspore as ms
 import mindspore.ops as ops
 import numpy as np
-import time
 
-ms.set_context(device_target="Ascend")
+ms.set_device(device_target="Ascend")
 
 A = ms.Tensor(np.ones((2048, 2048), np.float32))
 B = ms.Tensor(np.ones((2048, 1), np.float32))
-
-start = time.time()
+              
 C = ops.MatMul()(A, B)
-end = time.time()
-
-print("MindSpore Ascend Time:", (end - start) * 1000, "ms")
+sum = 0.0
+for _ in range(10000):
+    start = time.time()
+    C = ops.MatMul()(A, B)
+    end = time.time()
+    sum += (end - start) * 1000
+print("MindSpore Ascend MatVec Average Time:", sum / 10000, "ms")
